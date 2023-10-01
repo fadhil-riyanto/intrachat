1b47adf1fcd9038e63bbef74258c4302febd89c7
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2023-10-01 20:30:01 +0700

    fix getopt

--- main.c
+++ main.c
@@ -12,17 +12,33 @@
 static const struct option long_options[] = {
     {"addr",    getopt_required_arg,NULL,'a'},
     {"port",    getopt_required_arg,NULL,'p'},
-    {"nonblock",getopt_optional_arg,NULL,'b'},  
     {NULL,      0,                  0,   0}
 };
 
-int try_parse(int argc, char *argv[], struct server_parameter* server_parameter)
+void show_help()
+{
+    char help[1024] = "intrachat help\n\n" \
+                        "--addr: set server listen (required)\n" \
+                        "--port: set server listen port (required)\n" \
+                        "\n\n" \
+                        "example: ./server --addr 127.0.0.1 --port 9000\n";
+
+    printf("%s", help);
+}
+
+
+int _try_parse(int argc, char *argv[], struct server_parameter* server_parameter)
 {
     int ret;
     int longindex;
 
-    ret = getopt_long(argc, argv, "a:p:b", long_options, NULL);
-
+    ret = getopt_long(argc, argv, "a:p", long_options, &longindex);
+    //printf("ret %c", ret);
+    if (ret == -1) {
+        printf("server addr and port is required\n");
+        show_help();
+        exit(-1);
+    }
     switch (ret) {
         case 'a':
             server_parameter->addr = optarg;
@@ -38,15 +54,20 @@ int try_parse(int argc, char *argv[], struct server_parameter* server_parameter)
             return EINVAL;
     }
 
-    // if (server_parameter->addr == NULL || server_parameter->port == NULL) {
-    //     printf("server port and addr is required");
+    // if (!server_parameter->port && !server_parameter->port) {
+    //     printf("server addr and port is required");
     //     exit(EXIT_FAILURE);
     // }
-
-
-
-
+    return 0;
 }
+
+void try_parse(int argc, char *argv[], struct server_parameter* server_parameter)
+{
+    for(int i = 0; i < 2; i++) {
+        _try_parse(argc, argv, server_parameter);
+    }
+}
+
 
 void* callme(void *ptr)
 {
@@ -59,6 +80,8 @@ int main(int argc, char *argv[])
     struct server_parameter server_parameter = {NULL, NULL, NULL};
 
     try_parse(argc, argv, &server_parameter);
+
+    printf("starting at %s:%s", server_parameter.addr, server_parameter.port);
     create_udp_server(&server_parameter, callme);
 
     
