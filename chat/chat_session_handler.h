27fae2c3dc1ab467709f4b8d05aa610872989097
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2023-11-22 20:21:52 +0700

    change the parameter options up to 20

--- chat/chat_session_handler.h
+++ chat/chat_session_handler.h
@@ -8,7 +8,26 @@
 struct messange_input_properties
 {
     char *command;
-    char *value[];
+    char *p1;
+    char *p2;
+    char *p3;
+    char *p4;
+    char *p5;
+    char *p6;
+    char *p7;
+    char *p8;
+    char *p9;
+    char *p10;
+    char *p11;
+    char *p12;
+    char *p13;
+    char *p14;
+    char *p15;
+    char *p16;
+    char *p17;
+    char *p18;
+    char *p19;
+    char *p20;
 };
 
 struct messange_input
